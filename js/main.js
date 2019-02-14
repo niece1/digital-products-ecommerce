8abf778fbbf70fd02ad836c27bd3ea1ee6b3004a
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2019-02-14 23:57:36 +0200

    About and Portfolio Sections added

--- js/main.js
+++ js/main.js
@@ -4,60 +4,104 @@ $(document).ready(function () {
 		infinite:true,
 		draggable: false,
 	//	fade: true,
-		dots: false,
-		arrows: true,
-		autoplay: true,
-		speed: 900,
-		autoplaySpeed: 4000,
-		pauseOnDotsHover:true,
-		pauseOnHover:false,
-		cssEase: 'ease',
+	dots: false,
+	arrows: true,
+	autoplay: true,
+	speed: 900,
+	autoplaySpeed: 4000,
+	pauseOnDotsHover:true,
+	pauseOnHover:false,
+	cssEase: 'ease',
  // vertical: true,
-  prevArrow: $('#left_arrow'),
-  nextArrow: $('#right_arrow')
+ prevArrow: $('#left_arrow'),
+ nextArrow: $('#right_arrow')
+});
+
+	block_pos = $('.about_box_item').offset().top;
+	wrap_pos = $('.about_wrapper').offset().top; 
+	block_height = $('.about_box_item').outerHeight(); 
+	wrap_height = $('.about_wrapper').outerHeight(); 
+	block_width = $('.about_box_item').outerWidth(); 
+	pos_absolute = wrap_pos + wrap_height - block_height;
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > pos_absolute) {
+
+			$('.about_box_item').css({
+				'position': 'absolute',
+				'top': wrap_height - block_height,
+				'width': block_width
+			});
+		}
+		else if ($(window).scrollTop() > block_pos) { 
+
+			$('.about_box_item').css({
+				'position': 'fixed',
+				'top': '0px',
+				'width': block_width
+			}); 
+		} else {
+
+			$('.about_box_item').css({
+				'position': 'static'
+			}); 
+		}
+	});
+
+//Isotope
+var $grid = $('.portfolio_container').isotope({
+	itemSelector: '.portfolio_item',
+	stagger: 30
+});
+
+$('.filter_button_group').on( 'click', 'li', function() {
+	var filterValue = $(this).attr('data-filter');
+	$grid.isotope({ filter: filterValue });
+
+	$('.filter_button_group li').removeClass('active');
+	$(this).addClass('active');
 });
 
 	//Parallax
-    var $window = $(window);
-    if($('div[data-type="background"]').length){
-        $('div[data-type="background"]').each(function(){
+	var $window = $(window);
+	if($('div[data-type="background"]').length){
+		$('div[data-type="background"]').each(function(){
 
-            var $obj = $(this);
-            var offset = $obj.offset().top;
+			var $obj = $(this);
+			var offset = $obj.offset().top;
 
-            $(window).scroll(function()
-            {
-                offset = $obj.offset().top;
+			$(window).scroll(function()
+			{
+				offset = $obj.offset().top;
 
-                if ($window.scrollTop() > (offset - window.innerHeight))
-                {
-                    var yPos = -(($window.scrollTop() - offset) / 2 );
-                    var coords = '50% ' + ( yPos ) + 'px';
-                    $obj.css({ backgroundPosition:  coords });
-                }
-            });
-            $(window).resize(function()
-            {
-                offset = $obj.offset().top;
-            });
-        });
-    }
+				if ($window.scrollTop() > (offset - window.innerHeight))
+				{
+					var yPos = -(($window.scrollTop() - offset) / 2 );
+					var coords = '50% ' + ( yPos ) + 'px';
+					$obj.css({ backgroundPosition:  coords });
+				}
+			});
+			$(window).resize(function()
+			{
+				offset = $obj.offset().top;
+			});
+		});
+	}
 
 });
 
 (function() {
 	var triggerBttn = document.getElementById( 'trigger-overlay' ),
-		overlay = document.querySelector( 'div.overlay' ),
-		closeBttn = overlay.querySelector( 'button.overlay-close' );
-		transEndEventNames = {
-			'WebkitTransition': 'webkitTransitionEnd',
-			'MozTransition': 'transitionend',
-			'OTransition': 'oTransitionEnd',
-			'msTransition': 'MSTransitionEnd',
-			'transition': 'transitionend'
-		},
-		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-		support = { transitions : Modernizr.csstransitions };
+	overlay = document.querySelector( 'div.overlay' ),
+	closeBttn = overlay.querySelector( 'button.overlay-close' );
+	transEndEventNames = {
+		'WebkitTransition': 'webkitTransitionEnd',
+		'MozTransition': 'transitionend',
+		'OTransition': 'oTransitionEnd',
+		'msTransition': 'MSTransitionEnd',
+		'transition': 'transitionend'
+	},
+	transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+	support = { transitions : Modernizr.csstransitions };
 
 	function toggleOverlay() {
 		if( classie.has( overlay, 'open' ) ) {
